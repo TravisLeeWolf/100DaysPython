6b7900355e2b11dcff5ca072666a787427750c2f
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2021-07-06 10:18:30 +0900

    Day 1: Variables

---
+++ Code Days/Day 1/variables.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+# storeA is created to hold the value of a as it is changed to the value of b
+# Then b can take the value of storeA which is a original value
+storeA = a
+a = b
+b = storeA
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
