788013e42fc49d3cc7d6665731fb8c27fe449a8d
Travis Naidoo <42667524+TravisLeeWolf@users.noreply.github.com>
2021-07-10 17:00:08 +0900

    Day 8: prime number checker

---
+++ Code Days/Day 8/prime_num_checker.py
@@ -0,0 +1,22 @@
+#Write your code below this line 👇
+def prime_checker(number):
+   # Divide every number which is less than that number
+   is_prime = True
+   for i in range(number + 1):
+       if 1 < i < number:
+            if number % i == 0:
+                is_prime = False
+   if is_prime:
+       print("It's a prime number.")
+   else:
+       print("It's not a prime number.")
+
+
+
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
+
