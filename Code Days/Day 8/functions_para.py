dee4c9a4106fcecc9fa6ef2539ea9b69d08ce46e
Travis Naidoo <42667524+TravisLeeWolf@users.noreply.github.com>
2021-07-10 16:23:56 +0900

    Day 8: functions w parameters

---
+++ Code Days/Day 8/functions_para.py
@@ -0,0 +1,9 @@
+def greet_with(name, location):
+    print(f"Hello {name}.")
+    print(f"How's the weather in {location}?")
+
+# Positional Arguments
+greet_with("Bob", "Australia")
+
+# Keyword arguments
+greet_with(location="Canada", name="Jin")
