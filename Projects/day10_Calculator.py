13ae472789b993362ffce86af4b4fe2460667694
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2021-07-12 13:26:41 +0900

    Day 10: need to add multiple answers

--- Projects/day10_Calculator.py
+++ Projects/day10_Calculator.py
@@ -5,58 +5,49 @@ import day10_CalculatorArt
 # Add
 def add(a, b):
     """Returns the addition of two numbers"""
-    answer = a + b
-    return answer
+    return a + b
 
 # Subtract
 def subtract(a, b):
     """Returns the subtraction of two numbers"""
-    answer = a - b
-    return answer
+    return a - b
 
 # Multiply
 def multiply(a, b):
     """Returns the multiplication of two numbers"""
-    answer = a * b
-    return answer
+    return a * b
 
 # Divide
 def divide(a, b):
     """Returns the division of two numbers"""
     if b != 0:
-        answer = a / b
-        return answer
+        return a / b
     else:
         print("Can't divide by 0!")
+
+# Dictionary of operations and their function names
+operators = {"+" : add,
+            "-" : subtract,
+            "*" : multiply,
+            "/" : divide}
 
 # Get numbers and operation
 def get_numbers_and_operation():
     """Gets the number input and operator to use, first number then operation and last number"""
     first_number = float(input("What's the first number?: "))
-    print("+\n-\n*\n/")
+    for symbol in operators:
+        print(symbol)
     operation = input("Pick an operation: ")
     last_number = float(input("What's the next number?: "))
+    
+    # Perform calculation
+    answer = operators[operation](first_number, last_number)
+    
+    return f"{first_number} {operation} {last_number} = {answer}"
 
-    print(f"{first_number} {operation} {last_number} = ")
-    return first_number, operation, last_number
-
-# Perform calculation using numbers and operation
-def perform_calculation(first_number, operation, last_number):
-    """Takes the numbers provided and operator and assigns which calculation to perform"""
-    if operation == "+":
-        add(first_number, last_number)
-    elif operation == "-":
-        subtract(first_number, last_number)
-    elif operation == "*":
-        multiply(first_number, last_number)
-    elif operation == "/":
-        divide(first_number, last_number)
-    else:
-        print("An error ocurred.")
 
 # Start here
 print(day10_CalculatorArt.logo)
 
 # First run
-first_number, operation, second_number = get_numbers_and_operation()
-print(perform_calculation(first_number, operation, second_number))
+print(get_numbers_and_operation())
