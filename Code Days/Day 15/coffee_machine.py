a372d374f32af61485fcddd4b21d833abf8bca76
Travis Naidoo <42667524+TravisLeeWolf@users.noreply.github.com>
2021-07-18 15:46:31 +0900

    Day 15: working on it

--- Code Days/Day 15/coffee_machine.py
+++ Code Days/Day 15/coffee_machine.py
@@ -7,3 +7,27 @@ Program requirements
 4. Check if the transaction was successful
 5. Make coffee
 """
+WATER = resources["water"]
+MILK = resources["milk"]
+COFFEE = resources["coffee"]
+MONEY = 0
+
+# Print report
+def report():
+    return f"Water: {WATER}ml\nMilk: {MILK}ml\nCoffee: {COFFEE}g\nMoney: ${MONEY}"
+
+# Take order
+order = input("What would you like? (espresso/latte/cappuccino): ")
+if order == "report":
+    print(report())
+elif order not in ["espresso", "latte", "cappuccino"]:
+    print("This machine doesn't offer that, sorry.")
+
+item = menu[order]["ingredients"]
+item_cost = menu[order]["cost"]
+
+print(item)
+
+# Check if resources are sufficient
+
+# Process 
