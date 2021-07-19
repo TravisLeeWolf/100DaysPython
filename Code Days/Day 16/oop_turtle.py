c3de539855f11d5c1b9e82cf171288db443e4c7c
Travis Naidoo <42667524+TravisLeeWolf@users.noreply.github.com>
2021-07-19 20:45:21 +0900

    Day 16: learning oop with turtle

---
+++ Code Days/Day 16/oop_turtle.py
@@ -0,0 +1,11 @@
+# Made by TravisLeeWolf
+
+import turtle
+
+bob = turtle.Turtle()
+bob.shape("turtle")
+bob.color("ForestGreen")
+bob.forward(100)
+
+my_screen = turtle.Screen()
+my_screen.exitonclick()
