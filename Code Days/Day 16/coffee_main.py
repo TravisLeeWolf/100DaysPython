dd6421458088eff18c6f5dc77b876518f0866580
Travis Naidoo <42667524+TravisLeeWolf@users.noreply.github.com>
2021-07-20 08:01:48 +0900

    Day 16: updated after video

--- Code Days/Day 16/coffee_main.py
+++ Code Days/Day 16/coffee_main.py
@@ -2,18 +2,23 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-my_order = input("What would you like to order? 'latte', 'espresso' or 'cappuccino': ")
+
 drink_from_menu = Menu()
-
-my_drink = drink_from_menu.find_drink(my_order)
-
 coffee_machine = CoffeeMaker()
 payment_machine = MoneyMachine()
 
-if coffee_machine.is_resource_sufficient(my_drink):
-    if payment_machine.make_payment(my_drink.cost):
-        coffee_machine.make_coffee(my_drink)
+is_on = True
+
+while is_on:
+    my_order = input("What would you like to order? 'latte', 'espresso' or 'cappuccino': ")
+
+    if my_order == "off":
+        is_on = False
+    elif my_order == "report":
+        print(coffee_machine.report())
+        print(payment_machine.report())
     else:
-        print("Not enough money, sorry.")
-else:
-    print("Out of ingredients, sorry.")
+        my_drink = drink_from_menu.find_drink(my_order)
+        if coffee_machine.is_resource_sufficient(my_drink) and payment_machine.make_payment(my_drink.cost):
+            coffee_machine.make_coffee(my_drink)
+    
