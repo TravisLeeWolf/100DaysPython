5fae5f6cf6575dda2ecf3313dda5133d0df8512b
Travis Naidoo <42667524+TravisLeeWolf@users.noreply.github.com>
2021-07-20 20:46:19 +0900

    Day 17: added method

--- Code Days/Day 17/firstClass.py
+++ Code Days/Day 17/firstClass.py
@@ -4,9 +4,18 @@ class User:
         self.id = user_id
         self.username = username
         self.followers = 0
+        self.following = 0
+
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 user_1 = User("001", "Bob")
 user_2 = User("002", "Mary")
 
 print(user_1.id)
+print(user_2.followers)
+
+user_1.follow(user_2)
 print(user_2.followers)
