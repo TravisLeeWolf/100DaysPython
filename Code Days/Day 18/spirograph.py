d983b83e42c2d52fc31678a026db502622b49725
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2021-07-21 10:14:28 +0900

    Day 18: added function

--- Code Days/Day 18/spirograph.py
+++ Code Days/Day 18/spirograph.py
@@ -10,16 +10,25 @@ def random_color():
     color = (r, g, b)
     return color
 
-tim.speed(0)
+tim.speed("fastest")
 tim.pensize(1.2)
 
 ########### Challenge 5 - Spirograph ########
-direction = 0
-for _ in range(37):
-    tim.pencolor(random_color())
-    tim.circle(100)
-    tim.setheading(direction)
-    direction += 10
+# method without different degrees
+# direction = 0
+# for _ in range(37):
+#     tim.pencolor(random_color())
+#     tim.circle(100)
+#     tim.setheading(direction)
+#     direction += 10
+
+def draw_spirograph(degrees):
+    for _ in range(int(360 / degrees)):
+        tim.pencolor(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + degrees)
+
+draw_spirograph(5)
 
 screen = t.Screen()
 screen.exitonclick()
