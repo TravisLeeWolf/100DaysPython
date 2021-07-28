eeaacc703ddd4ceb94d99ee7b09967aed004e329
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2021-07-28 15:35:05 +0900

    Day 24: mail merge

--- Code Days/Day 24/Mail Merge Project Start/main.py
+++ Code Days/Day 24/Mail Merge Project Start/main.py
@@ -2,6 +2,25 @@
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
+
+startingLetterLines = []
+
+with open(".\Input\Letters\starting_letter.txt") as startingLetter:
+    startingLetterLines = startingLetter.readlines()
+
+with open(r".\Input\Names\invited_names.txt") as invitedNames:
+    invitedNamesData = invitedNames.readlines()
+    invitedNamesList = []
+    for name in invitedNamesData: 
+        invitedNamesList.append(name.rstrip())
+
+for name in invitedNamesList:
+    finalLetterText = startingLetterLines[0].replace("[name]", name)
+    with open(f".\Output\ReadyToSend\letter_to_{name}.txt", mode="w") as outputLetter:
+        for i in range(1, len(startingLetterLines)):
+            finalLetterText += startingLetterLines[i]
+        outputLetter.write(finalLetterText)
+
     
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
