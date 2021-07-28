538aa394a1970ae907eb828142e1b186b1b63d44
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2021-07-28 14:24:18 +0900

    Day 24: mail merge start

--- Code Days/Day 24/snake_w_highscore/scoreboard.py
+++ Code Days/Day 24/snake_w_highscore/scoreboard.py
@@ -8,13 +8,15 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highScore = 0
+        with open("data.txt") as dataFile:
+            self.highScore = int(dataFile.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
         self.updateScoreboard()
-
+        
+    
     def updateScoreboard(self):
         self.clear()
         self.write(f"Score: {self.score} High Score: {self.highScore}", align=ALIGNMENT, font=FONT)    
@@ -22,6 +24,8 @@ class ScoreBoard(Turtle):
     def resetScore(self):
         if self.score > self.highScore:
             self.highScore = self.score
+            with open("data.txt", mode="w") as dataFile:
+                dataFile.write(str(self.highScore))
         self.score = 0
         self.updateScoreboard()
     
