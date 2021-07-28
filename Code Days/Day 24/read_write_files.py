572ce46b692be7a30c39ee8868b63c23bcc07d33
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2021-07-28 14:00:37 +0900

    Day 24: writing files

--- Code Days/Day 24/read_write_files.py
+++ Code Days/Day 24/read_write_files.py
@@ -1,3 +1,12 @@
-with open("myText.txt") as myFile:
-    contents = myFile.read()
-    print(contents)
+# with open("myText.txt") as myFile:
+#     contents = myFile.read()
+#     print(contents)
+
+# with open("myText.txt", mode="w") as myFile:
+#     myFile.write("This overwrites the content of the file.")
+
+# with open("myText.txt", mode="a") as myFile:
+#     myFile.write("\nThis text has been appended to this file.")
+
+with open("newText.txt", mode="w") as myFile:
+    myFile.write("In the write mode if the file doesn't exist a new file will be created.")
