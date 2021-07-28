368cab6eb3b2af66194b8016afd8355f7d6b6278
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2021-07-28 09:55:19 +0900

    Day 23: working on car generation

--- Code Days/Day 23/car_manager.py
+++ Code Days/Day 23/car_manager.py
@@ -1,7 +1,31 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_wid=1, stretch_len=2)
+        self.setheading(180)
+        self.penup()
+        self.pickRandomBodyColor()
+        self.startAtRandomYPosition()
+        self.moveForward()
+
+    def pickRandomBodyColor(self):
+        bodyColor = random.choice(COLORS)
+        self.color(bodyColor)
+
+    def startAtRandomYPosition(self):
+        yPosition = random.randrange(-240, 240, 20)
+        self.goto(300, yPosition)
+
+
+    def moveForward(self):
+        self.forward(STARTING_MOVE_DISTANCE)
--- Code Days/Day 23/main.py
+++ Code Days/Day 23/main.py
@@ -6,9 +6,34 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.title("Turtle Crossing")
 screen.tracer(0)
 
-game_is_on = True
-while game_is_on:
+# Instance in the player
+player = Player()
+
+# Listen for player input
+screen.listen()
+screen.onkeypress(player.moveForward, "Up")
+
+# Game loop
+gameIsOn = True
+gameLoopCount = 0
+
+carsOnRoad = []
+def moveAllCars(carList):
+    for car in carList:
+        car.moveForward()
+
+while gameIsOn:
     time.sleep(0.1)
     screen.update()
+    # Randomly instance in cars, NOTE: new car is spawned every 6th game loop
+    if gameLoopCount == 6:
+        newCar = CarManager()
+        carsOnRoad.append(newCar)
+        moveAllCars(carsOnRoad)
+        gameLoopCount = 0
+    else:
+        gameLoopCount += 1
+        moveAllCars(carsOnRoad)
