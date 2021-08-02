23a33d9a6d0527baddda38011107088524cc275a
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2021-08-02 14:39:24 +0900

    Project: added UI elements

--- Projects/Day 28 - Pomodoro/main.py
+++ Projects/Day 28 - Pomodoro/main.py
@@ -25,12 +25,23 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomatoImage = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomatoImage)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
 
+# Title Text
+titleLabel = Label(text="Timer", font=(FONT_NAME, 35, "bold",), fg=GREEN, bg=YELLOW)
+titleLabel.grid(column=1, row=0, sticky="s")
 
+# Start button
+startButton = Button(text="Start", font=(FONT_NAME, 15))
+startButton.grid(column=0, row=2)
 
+# Reset button
+resetButton = Button(text="Reset", font=(FONT_NAME, 15))
+resetButton.grid(column=2, row=2)
 
-
+# 25 min completed label ⏱🍅
+pomoDoneLabel = Label(text="[⏱]", font=(FONT_NAME, 20, "bold",), bg=YELLOW )
+pomoDoneLabel.grid(column=1, row=3)
 
 
 # Goes at the end so the window doesn't automatically close
