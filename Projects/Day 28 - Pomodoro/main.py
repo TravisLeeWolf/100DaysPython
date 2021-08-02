c0b1ab3125a43cb9eb59bdece3d3dd198f7d3dc2
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2021-08-02 13:59:24 +0900

    Day 28: working on UI

--- Projects/Day 28 - Pomodoro/main.py
+++ Projects/Day 28 - Pomodoro/main.py
@@ -1,4 +1,4 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +16,22 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("My Pomodoro App")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+# Adding the tomato picture to our window
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomatoImage = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomatoImage)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.pack()
+
+
+
+
+
+
+
+# Goes at the end so the window doesn't automatically close
+window.mainloop()
