cc2efcfd9fc06a2645b57303c9f92800f7e8af9d
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2021-08-03 15:58:50 +0900

    Day 29: working on password generator

--- Code Days/Day 29/main.py
+++ Code Days/Day 29/main.py
@@ -1,6 +1,25 @@
 from tkinter import *
 from tkinter.messagebox import askokcancel, showwarning
+from random import randint, choice, shuffle
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+#Password Generator Project
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+passwordLetterList = [choice(letters) for _ in range(randint(8, 10))]
+passwordSymbolList = [choice(symbols) for _ in range(randint(2, 4))]
+passwordNumberList = [choice(numbers) for _ in range(randint(2, 4))]
+
+passwordList = passwordLetterList + passwordSymbolList + passwordNumberList
+
+shuffle(passwordList)
+
+password = ""
+for char in passwordList:
+  password += char
+
+print(f"Your password is: {password}")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
