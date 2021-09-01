23811a36fb6cd1d2592159043f24797f39edfe58
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2021-09-01 12:20:38 +0900

    Day 45: working with bs4

---
+++ Code Days/Day 45/bs4_start.py
@@ -0,0 +1,22 @@
+from turtle import heading
+from bs4 import BeautifulSoup
+
+with open("website.html", encoding="utf-8") as myFile:
+    contents = myFile.read()
+
+soup = BeautifulSoup(contents, "html.parser")
+
+# print(soup.title)
+# print(soup.prettify())
+
+allAnchorTags = soup.find_all(name="a")
+for tag in allAnchorTags:
+    # print(tag.getText())
+    print(tag.get("href"))
+
+# mainHeading = soup.find(name="h1", id="name")
+# print(mainHeading)
+
+classIsHeading = soup.find(class_="heading") # NOTE: use class_ to avoid a conflict with Python class method
+print(classIsHeading.getText())
+
