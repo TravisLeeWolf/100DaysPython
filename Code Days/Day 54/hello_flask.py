4172a6eb8568e899093ea9cf5abc2f9e638e7625
Travis Naidoo <42667524+TravisLeeWolf@users.noreply.github.com>
2021-09-05 17:03:44 +0900

    Day 54: running, check comments for how to

--- Code Days/Day 54/hello_flask.py
+++ Code Days/Day 54/hello_flask.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+# NOTE: Setting up the environment using powershell
+## py -m venv env
+## env\Scripts\activate
+## pip install flask
+## $env:FLASK_APP = "program.py"
+## flask run
+
+## Ctrl + C (To close the server)
