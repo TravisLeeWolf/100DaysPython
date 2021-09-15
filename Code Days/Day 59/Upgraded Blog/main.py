187a19445f4ff611cecdf55e9041298bff7f6336
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2021-09-15 13:20:10 +0900

    Day 60: test emailer

--- Code Days/Day 59/Upgraded Blog/main.py
+++ Code Days/Day 59/Upgraded Blog/main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
 import requests
 from post import Post
+import smtplib
+
+EMAIL_ADDRESS = ""
+EMAIL_PASSWORD = ""
 
 POSTS_URL = "https://api.npoint.io/88c2c1f644ef334058be"
 posts = requests.get(POSTS_URL).json()
@@ -27,18 +31,33 @@ def showPost(index):
 def showAbout():
     return render_template("about.html")
 
-@app.route('/contact')
-def showContact():
-    return render_template("contact.html")
+def sendMail(name, email, phone, message):
+    message = f"{name} sent you an message.\n{message}\nYou can contact them at {email} or {phone}."
+    try:
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=EMAIL_ADDRESS, password=EMAIL_PASSWORD)
+            connection.sendmail(from_addr=EMAIL_ADDRESS,
+                                to_addrs=EMAIL_ADDRESS,
+                                msg=f"Subject:Message from {name}\n\n{message}")
+    except:
+        return "Issue with sending message."
+    else:
+        return "Message sent successfully!"
 
-@app.route('/sent', methods=["POST"])
-def sendMessage():
-    data = request.form
-    print(data["name"])
-    print(data["email"])
-    print(data["phone"])
-    print(data["message"])
-    return "<h1>Successfully sent your message</h1>"
+@app.route("/contact", methods=["GET", "POST"])
+def contactPage():
+    if request.method == "POST":
+        data = request.form
+        name = data["name"]
+        email = data["email"]
+        phone = data["phone"]
+        message = data["message"]
+        pageMessage = sendMail(name=name, email=email, phone=phone, message=message)
+        return render_template("contact.html", contactMessage=pageMessage)
+    else:
+        pageMessage = "Contact Me"
+        return render_template("contact.html", contactMessage=pageMessage)
 
 if __name__ == "__main__":
     app.run(debug=True)
