104ef24fe7fe5fd2b509b9708ff28c3c8def160d
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2021-09-30 11:00:19 +0900

    working on day 61

--- Code Days/Day 61/forms.py
+++ Code Days/Day 61/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired
 
 class MyForm(FlaskForm):
     email = StringField('email', validators=[DataRequired()])
-    password = StringField('password', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
