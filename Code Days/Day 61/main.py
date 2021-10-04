b1b30cf595d1d8edf3fa995cfa165cd9c824c7d6
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2021-10-04 11:30:55 +0900

    Day 61: flask bootstrap

--- Code Days/Day 61/main.py
+++ Code Days/Day 61/main.py
@@ -1,18 +1,24 @@
 from flask import Flask, render_template
 from forms import MyForm
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-app.secret_key = 'secret'
+Bootstrap(app)
+app.secret_key = 'mysecret'
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = MyForm()
-    form.validate_on_submit()
-    return render_template('login.html', form=form)
+    if form.validate_on_submit():
+        if form.email.data == "admin@email.com" and form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template("login.html", form=form)
 
 
 if __name__ == '__main__':
