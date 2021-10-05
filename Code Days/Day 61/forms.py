1d5988e511140cba2da3dbebc7e68ea049a69bed
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2021-10-05 09:59:13 +0900

    Day 61 bootstrap wtf

--- Code Days/Day 61/forms.py
+++ Code Days/Day 61/forms.py
@@ -3,6 +3,6 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
 class MyForm(FlaskForm):
-    email = StringField(label='email', validators=[DataRequired(), Email()])
-    password = PasswordField(label='password', validators=[DataRequired(), Length(min=8)])
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
     submit = SubmitField(label="Log In")
